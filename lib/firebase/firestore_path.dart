026a92cf50af8b4387492d0d20d7d9030ce7d917
kurogoma939 <kan.sss.939@gmail.com>
2022-05-27 22:34:12 +0900

    Issue #7 Firestoreパスの作成

---
+++ lib/firebase/firestore_path.dart
@@ -0,0 +1,8 @@
+class FirestorePath {
+  FirestorePath._();
+
+  // Todoコレクション
+  static const String todosDomain = 'todos';
+  // Todoに対するサブコレクションの設定
+  // static String chatRoomPath(String todoId) => '$todosDomain/$todoId';
+}
