d2f6d449aa8ed698a3fd583eea0109e15c17bc4a
Kurogoma939 <kan.sss.939@gmail.com>
2022-06-03 10:23:44 +0900

    watchする部分だけ切り抜き

--- lib/ui/todo/todo_sample.dart
+++ lib/ui/todo/todo_sample.dart
@@ -7,15 +7,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // 🌎 Project imports:
 import '../todo/todo_list_view_model.dart';
 
-class SampleTodoPage extends ConsumerWidget {
+class SampleTodoPage extends StatelessWidget {
   const SampleTodoPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final viewModel = ref.watch(todoPageViewModelProvider);
-    final todoLevel =
-        ref.watch(todoPageViewModelProvider.select((todo) => todo.todoLevel));
-
+  Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -30,59 +26,92 @@ class SampleTodoPage extends ConsumerWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               const Text('カウンター', style: TextStyle(fontSize: 28)),
-              Text(viewModel.count.toString(),
-                  style: const TextStyle(fontSize: 20)),
-              SizedBox(
-                width: size.width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: Colors.blue,
-                      child: IconButton(
-                          onPressed: () => viewModel.subCounter(),
-                          icon: const Icon(Icons.exposure_minus_1_outlined,
-                              color: Colors.white)),
-                    ),
-                    CircleAvatar(
-                      backgroundColor: Colors.blue,
-                      child: IconButton(
-                          onPressed: () => viewModel.addCounter(),
-                          icon: const Icon(Icons.plus_one_outlined,
-                              color: Colors.white)),
-                    ),
-                  ],
-                ),
-              ),
+              const CounterComponent(),
               Container(width: size.width, height: 1, color: Colors.black38),
               const Text('Todoレベル', style: TextStyle(fontSize: 28)),
-              Text(todoLevel.toString(), style: const TextStyle(fontSize: 20)),
-              SizedBox(
-                width: size.width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: Colors.orange,
-                      child: IconButton(
-                          onPressed: () => viewModel.subTodoLevel(),
-                          icon: const Icon(Icons.exposure_minus_1_outlined,
-                              color: Colors.white)),
-                    ),
-                    CircleAvatar(
-                      backgroundColor: Colors.orange,
-                      child: IconButton(
-                          onPressed: () => viewModel.addTodoLevel(),
-                          icon: const Icon(Icons.plus_one_outlined,
-                              color: Colors.white)),
-                    ),
-                  ],
-                ),
-              ),
+              const TodoLevelComponent(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class CounterComponent extends ConsumerWidget {
+  const CounterComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModel = ref.watch(todoPageViewModelProvider);
+    Size size = MediaQuery.of(context).size;
+
+    return Column(
+      children: <Widget>[
+        Text(viewModel.count.toString(), style: const TextStyle(fontSize: 20)),
+        SizedBox(
+          width: size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: IconButton(
+                    onPressed: () => viewModel.subCounter(),
+                    icon: const Icon(Icons.exposure_minus_1_outlined,
+                        color: Colors.white)),
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: IconButton(
+                    onPressed: () => viewModel.addCounter(),
+                    icon: const Icon(Icons.plus_one_outlined,
+                        color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TodoLevelComponent extends ConsumerWidget {
+  const TodoLevelComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModel = ref.watch(todoPageViewModelProvider);
+    final todoLevel =
+        ref.watch(todoPageViewModelProvider.select((todo) => todo.todoLevel));
+    Size size = MediaQuery.of(context).size;
+
+    return Column(
+      children: <Widget>[
+        Text(todoLevel.toString(), style: const TextStyle(fontSize: 20)),
+        SizedBox(
+          width: size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.orange,
+                child: IconButton(
+                    onPressed: () => viewModel.subTodoLevel(),
+                    icon: const Icon(Icons.exposure_minus_1_outlined,
+                        color: Colors.white)),
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.orange,
+                child: IconButton(
+                    onPressed: () => viewModel.addTodoLevel(),
+                    icon: const Icon(Icons.plus_one_outlined,
+                        color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
